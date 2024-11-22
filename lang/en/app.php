0ad11b4c6e97539d7d992f91f6cbed13dce36d5d
Kwesi Danquah <kwescon1@gmail.com>
2024-11-22 02:42:48 +0000

    feat: publish language files for localization support

---
+++ lang/en/app.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    // feat: Add localization language lines for common application messages
+    /*
+    |--------------------------------------------------------------------------
+    | Application Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in application for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    // Application general messages
+    'unexpected_error' => 'Unexpected Error Occured!',
+    'operation_successful' => 'Operation successful!',
+    'resource_created' => 'Resource created!',
+    'resource_not_found' => 'Resource not found!',
+    'model_not_found' => ':model not found!',
+    'validation_failed' => 'Validation failed!',
+    'unexpected_error' => 'An unexpected error occurred',
+    'access_denied' => 'Access Denied!',
+];
