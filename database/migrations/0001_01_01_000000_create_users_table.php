15c53a51dc201a88ac3e3f0d304b47458a9726a4
Kwesi Danquah <kwescon1@gmail.com>
2024-11-22 03:36:49 +0000

    style: format codebase with Pint

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +18,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable(); // Make password nullable
+            $table->enum('role', ['admin', 'salesperson'])->default('salesperson');
             $table->rememberToken();
             $table->timestamps();
         });
