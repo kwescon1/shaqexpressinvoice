092579f9258ef5465c0c6ff768bd78e7f4cb3242
Kwesi Danquah <kwescon1@gmail.com>
2024-11-22 03:28:48 +0000

    chore: register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\BindingServiceProvider::class,
     App\Providers\ResponseServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
