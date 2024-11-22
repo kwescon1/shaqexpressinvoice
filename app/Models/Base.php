0fd4d0341c5a9b52ae61bd536c8ea28ae7b3eb6e
Kwesi Danquah <kwescon1@gmail.com>
2024-11-22 11:59:15 +0000

    feat: create BaseModel for shared model logic

---
+++ app/Models/Base.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class Base extends Model
+{
+    use HasUuids;
+
+    /**
+     * The attributes that should be hidden for arrays and JSON serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = ['id'];
+
+    /**
+     * Get the route key name for Laravel's model binding.
+     */
+    final public function getRouteKeyName(): string
+    {
+        return 'uuid';
+    }
+
+    /**
+     * Get the columns that should receive a unique identifier.
+     *
+     * @return array<int, string>
+     */
+    final public function uniqueIds(): array
+    {
+        return ['uuid'];
+    }
+}
